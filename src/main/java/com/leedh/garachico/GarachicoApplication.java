8fb6dc975af87b624b698227efb5a5bd810f87fe
wishadow <wishadow@gmail.com>
2018-07-06 23:54:00 +0900

    프로젝트 초기화

---
+++ src/main/java/com/leedh/garachico/GarachicoApplication.java
@@ -0,0 +1,12 @@
+package com.leedh.garachico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarachicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarachicoApplication.class, args);
+    }
+}
