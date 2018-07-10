c5128291761f4593888c433135c97b4a06992c40
ogredwarf <wishadow@gmail.com>
2018-07-10 18:40:22 +0900

    잘못 올린것 복구
    잘못 올린것 재복구... 이게 BookController 원본

--- src/main/java/com/leedh/garachico/controller/BookController.java
+++ src/main/java/com/leedh/garachico/controller/BookController.java
@@ -1,39 +1,100 @@
-<style>
-.detailInfo li{
-    list-style-type: none;
-}
-.detailView div.thumbnail
-{
-    text-align: center;
-}
+package com.leedh.garachico.controller;
 
-.detailInfo span.label {
-    display: inline-block;
-    width: 150px;
+import com.leedh.garachico.dto.BookDTO;
+import com.leedh.garachico.enums.Target;
+import com.leedh.garachico.service.feign.KakaoBookApiService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 설명:
+ * Project: garachico
+ * CLASS: BookController
+ * User: 이동훈
+ * Date: 2018-07-09
+ */
+
+@Slf4j
+@Controller
+public class BookController {
+
+    @Autowired
+    KakaoBookApiService kakaoBookApiService;
+
+    /**
+     * 책 검색 방안
+     */
+    @PostMapping("book/list")
+    public String find(HttpServletRequest request,
+                       HttpServletResponse response,
+                       ModelMap model,
+                       @RequestParam("query")                                                      String query,       /*검색을 원하는 질의어*/
+                       @RequestParam(value = "sort"    , required = false)                         String sort,        /*결과 문서 정렬 방식*/
+                       @RequestParam(value = "category", required = false)                         String category,    /*검색 필드 제한*/
+                       @RequestParam(value = "target"  , required = false)                         String target,      /*카테고리 필터링*/
+                       @RequestParam(value = "page"    , required = false, defaultValue = "1")     Integer pageNo,     /*결과 페이지 번호*/
+                       @RequestParam(value = "size"    , required = false, defaultValue = "10")    Integer pageSize    /*한 페이지에 보여질 문서의 개수*/
+    ){
+
+        log.debug("quert = {}", query);
+
+        BookDTO.Res result = kakaoBookApiService.findBook(query, sort, category, target, pageNo, pageSize );
+
+        model.put("result", result);
+        model.put("page", pageNo);
+
+        return "book/list";
+    }
+
+
+    @GetMapping("book/detail/{isbn}")
+    public String getDetail(HttpServletRequest request,
+                       HttpServletResponse response,
+                       ModelMap model,
+                       @PathVariable("isbn")                                                      String isbn       /*검색을 원하는 질의어*/
+    ){
+
+        log.debug("quert = {}", isbn);
+
+        BookDTO.Res result = kakaoBookApiService.findBookByIsbn(isbn, Target.ISBN.getCode() );
+
+        model.put("result", result);
+
+        return "book/detail";
+    }
+
+    /**
+     * DTO 방식
+     */
+    /**
+     * 책 검색 방안 1
+     */
+    @PostMapping("book/getListByDTO")
+    public String find(HttpServletRequest request,
+                       HttpServletResponse response,
+                       ModelMap model,
+                       @ModelAttribute BookDTO.Req param
+    ){
+
+        log.debug( "book/getList : param = {}", param );
+
+        String query        = param.getQuery();
+        String sort         = param.getSort();
+        String category     = param.getCategory();
+        String target       = param.getTarget();
+        Integer pageNo      = param.getPageNo();
+        Integer pageSize    = param.getPageSize();
+
+        BookDTO.Res result = kakaoBookApiService.findBook(query, sort, category, target, pageNo, pageSize );
+
+        model.put("result", result);
+
+        return "book/list";
+    }
 }
-</style>
-
-<div data-th-unless="${#lists.isEmpty(result.documents)}">
-    <div class="detailView" data-th-with="item=${result.documents[0]}">
-        <div class="thumbnail">
-            <a data-th-href="${item.url}">
-                <img data-th-src="${item.thumbnail}"/>
-            </a>
-
-        </div>
-        <div class="detailInfo">
-            <ul>
-                <li data-th-unless="${#lists.isEmpty(item.authors)}">     <span class="label">작가</span>  <span data-th-name="authors" data-th-text="${#strings.listJoin(item.authors, ',')}">authors</span> 저</li>
-                <li data-th-unless="${#lists.isEmpty(item.translators)}"> <span class="label">번역자</span>  <span data-th-name="translators" data-th-text="${#strings.listJoin(item.translators, ',')}">translators</span> 역</li>
-                <li data-th-unless="${#lists.isEmpty(item.publisher)}">   <span class="label">출판사</span>  <span data-th-name="publisher" data-th-text="${item.publisher}">publisher</span></li>
-                <li data-th-unless="${#lists.isEmpty(item.price)}">       <span class="label">도서 정가</span>  <span data-th-name="price" data-th-text="${item.price}">price</span>원</li>
-                <li data-th-unless="${#lists.isEmpty(item.salePrice)}">   <span class="label">도서판매가</span>  <span data-th-name="salePrice" data-th-text="${item.salePrice}">sale_price</span>원</li>
-                <li data-th-unless="${#lists.isEmpty(item.status)}">      <span class="label">책 상태</span>  <span data-th-name="status" data-th-text="${item.status}">status</span></li>
-                <li data-th-unless="${#lists.isEmpty(item.url)}">         <span class="label">다음책검색</span>  <span data-th-name="url"> <a data-th-href="${item.url}">링크</a></span></li>
-            </ul>
-        </div>
-    </div>
-</div>
-<div data-th-if="${#lists.isEmpty(result.documents)}">
-    검색 결과가 없습니다.
-</div>
