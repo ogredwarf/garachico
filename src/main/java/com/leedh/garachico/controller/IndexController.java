51db556aa947f49ed961e64cbffbc5f7df96268d
ogredwarf <wishadow@gmail.com>
2018-07-11 18:54:32 +0900

    index 컨트롤러 url 수정

--- src/main/java/com/leedh/garachico/controller/IndexController.java
+++ src/main/java/com/leedh/garachico/controller/IndexController.java
@@ -3,7 +3,8 @@ package com.leedh.garachico.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * 기본 페이지 라우팅
@@ -19,22 +20,23 @@ public class IndexController {
     /**
      * 기본 페이지 설정
      */
-    @GetMapping("/")
+    @RequestMapping("/")
     public String index() {
         log.debug("index");
         return "index";
     }
 
-    @GetMapping("/login")
+    @RequestMapping("/login")
     public String login() {
         log.debug("login");
         return "member/login";
     }
 
-    @GetMapping("/login-error")
-    public String login(Model model) {
+    @RequestMapping("/login/")
+    public String login(Model model,
+                        @RequestParam("fail") Boolean isLoginFail ) {
         log.debug("login-error");
-        model.addAttribute("loginError", true);
+        model.addAttribute("loginError", isLoginFail);
         return "member/login";
     }
 }
