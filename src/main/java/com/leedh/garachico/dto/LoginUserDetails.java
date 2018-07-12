94db24ed3d05e6444ffc5652af494300b5a14d94
ogredwarf <wishadow@gmail.com>
2018-07-12 18:32:10 +0900

    로그인용 사용자 설정

--- src/main/java/com/leedh/garachico/dto/LoginUserDetails.java
+++ src/main/java/com/leedh/garachico/dto/LoginUserDetails.java
@@ -1,8 +1,14 @@
 package com.leedh.garachico.dto;
 
 import com.leedh.garachico.entity.Member;
+import com.leedh.garachico.entity.MemberRole;
 import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 설명:
@@ -13,12 +19,21 @@ import org.springframework.security.core.userdetails.User;
  */
 public class LoginUserDetails extends User {
 
+    private static final String ROLE_PREFIX = "ROLE_";
+    private static final long serialVersionUID = 1L;
+
     @Getter
     private Long memberNo;
 
     public LoginUserDetails(Member member)
     {
-        super( member.getUsername(), member.getPassword(), member.getMemberRoles());
+        super( member.getUsername(), member.getPassword(), makeGrantedAuthority(member.getRoles()));
         this.memberNo = member.getMemberNo();
+    }
+
+    private static List<GrantedAuthority> makeGrantedAuthority(List<MemberRole> roles){
+        List<GrantedAuthority> list = new ArrayList<>();
+        roles.forEach(role -> list.add(new SimpleGrantedAuthority(ROLE_PREFIX + role.getRoleName())));
+        return list;
     }
 }
