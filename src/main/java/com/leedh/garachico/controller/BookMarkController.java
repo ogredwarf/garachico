8388a040969662b0e37bef2912c4c31a4f37580d
wishadow <wishadow@gmail.com>
2018-07-15 16:37:12 +0900

    북마크 정렬 기능 추가 - 시간순 역순으로 정렬(최근 등록일자 기준 ) - 책제목순 정렬

--- src/main/java/com/leedh/garachico/controller/BookMarkController.java
+++ src/main/java/com/leedh/garachico/controller/BookMarkController.java
@@ -11,8 +11,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 설명: 북마크 조회, 추가 삭제 관련
@@ -29,19 +32,32 @@ public class BookMarkController {
     @Autowired
     BookMarkService bookMarkService;
 
+
     /**
-     * 책 검색 결과 조회
+     * 책 검색 결과 조회 조회 조건 추가
      * @param model
      * @return
      */
     @RequestMapping("list")
-    public String list( Model model){
+    public String list(Model model,
+                       @RequestParam(value = "sort", required = false, defaultValue = "title") final String sort ){
 
         /* 로그인한 사용자 정보 가져오기*/
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         Member loginedMember = ((LoginUserDetails) auth.getPrincipal()).getMember();
 
         List<BookMark> list = bookMarkService.list(loginedMember);
+
+        if(sort.equals("title")){
+            list = list.stream()
+                    .sorted( Comparator.comparing(BookMark::getTitle))
+                    .collect(Collectors.toList());
+        }
+        else {
+            list = list.stream()
+                    .sorted( Comparator.comparing(BookMark::getCreateDt).reversed())
+                    .collect(Collectors.toList());
+        }
 
         model.addAttribute("bookmarkList", list);
         return "/bookmark/list";
